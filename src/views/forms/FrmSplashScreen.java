72facc456ffcd85543c42df3ec187c4014ac4a8f
Wishva Kalhara <vishvakalhara@gmail.com>
2024-10-18 20:34:17 +0530

    app layout

--- src/views/forms/FrmSplashScreen.java
+++ src/views/forms/FrmSplashScreen.java
@@ -4,6 +4,9 @@
  */
 package views.forms;
 
+import com.formdev.flatlaf.themes.FlatMacLightLaf;
+import views.layouts.AppLayout;
+
 /**
  *
  * @author vishv
@@ -47,32 +50,14 @@ public class FrmSplashScreen extends javax.swing.JFrame {
      */
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(FrmSplashScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(FrmSplashScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(FrmSplashScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(FrmSplashScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
+        FlatMacLightLaf.registerCustomDefaultsSource("styles");
+        FlatMacLightLaf.setup();
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new FrmSplashScreen().setVisible(true);
+//                new FrmSplashScreen().setVisible(true);
+                new AppLayout().setVisible(true);
             }
         });
     }
